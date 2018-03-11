72f9559822a58a61c12a2892d119cd9aced2905c
Kamyar Nadjafloo <knadjafloo@gmail.com>
2018-03-10 21:05:06 -0800

    added social login with google and facebook using amazon amplify

---
+++ src/Home.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import FormElement from './FormElement';
+import { FormGroup, Button } from 'react-bootstrap';
+
+
+export default class Home extends Component {
+
+
+
+    render() {
+        return (
+            <div>
+                <h3>
+                    Home page.
+                </h3>
+                <p>
+                    hello world
+                    </p>
+            </div>
+        );
+    }
+}
